9609b93ae892109f8046d9139148406038434fa7
Gautier Chaigneau <gautier.chaigneau@gmail.com>
2018-05-12 19:05:46 +0200

    pdf

--- api/controllers/ProduitController.js
+++ api/controllers/ProduitController.js
@@ -226,12 +226,184 @@ module.exports = {
 	},
     
     'devis': function(req, res, next){
-
-        pdf.create("<html><body><h1 style='color:red'>COUCOU les ZAMIS</h1></body></html>").toStream((err, stream) => {
-            if (err) return res.end(err.stack)
-            res.setHeader('Content-type', 'application/pdf')
-            stream.pipe(res)
-        });
+        Projet.findOne(req.param('id')).populate("cd_client").exec(function(err, projet){
+            if(err){
+                return next(err);
+            }
+            Produit.find({cd_projet: req.param('id')}).populate("cd_gamme").exec(function(err, produits){
+                if(err){
+                    return next(err);
+                }
+                Produit_module.find().populate("cd_module").exec(function(err, produits_module){
+                   if(err){
+                        return next(err);
+                    } 
+                    Composant.find().exec(function(err, composants){
+                       if(err){
+                            return next(err);
+                        } 
+                        Module_composant.find().populate("cd_composant").exec(function(err, modules_composant){
+                           if(err){
+                                return next(err);
+                            }
+                            var html = "";
+                            html += "<html><body><h2 style='text-align:center'>Devis Quantitatif et Estimatif</h2>";
+                            html += "<h4>Projet</h4>";
+                            html += "Référence: <i style='color:blue'>" + projet.id + "</i><br/>";
+                            html += "Projet: <i style='color:blue'>" + projet.titre + "</i><br/>";
+                            var date = new Date(projet.date_creation);
+                            html += "Date: <i style='color:blue'>" + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "</i><br/>";
+                            html += "<h4>Client</h4>";
+                            html += "Nom: <i style='color:blue'>" + projet.cd_client.nom + "</i><br/>";
+                            html += "Prenom: <i style='color:blue'>" + projet.cd_client.prenom + "</i><br/>";
+                            html += "Mail: <i style='color:blue'>" + projet.cd_client.mail + "</i><br/>";
+                            html += "Tel. fixe: <i style='color:blue'>" + projet.cd_client.telephone + "</i><br/>";
+                            html += "Tel. portable: <i style='color:blue'>" + projet.cd_client.portable + "</i><br/>";
+                            html += "Adresse: <i style='color:blue'>" + projet.cd_client.adresse_numero + ", " + projet.cd_client.adresse + " " + projet.cd_client.adresse_codepostale + " " + projet.cd_client.adresse_ville + "</i><br/>";
+                            html += "<h4>Produits</h4>";
+                            var tarif = 0;
+                            for(var i = 0; i < produits.length; i++){
+                                html += "Produit: <i style='color:blue'>" + produits[i].note + "</i><br/>";
+                                html += "CCTP: <i style='color:blue'>Lorem Ipsum</i><br/>";
+                                for(var u = 0; u < composants.length; u++){
+                                    switch(composants[u].id){
+                                        case produits[i].cd_gamme.finition_exterieur:
+                                             html += "Finition exterieur: <i style='color:blue'>" + composants[u].designation + "</i><br/>";
+                                            break;
+                                        case produits[i].cd_gamme.finition_interieur:
+                                             html += "Finition interieur: <i style='color:blue'>" + composants[u].designation + "</i><br/>";
+                                            break;
+                                        case produits[i].cd_gamme.isolant:
+                                             html += "Isolation: <i style='color:blue'>" + composants[u].designation + "</i><br/>";
+                                            break;
+                                        case produits[i].cd_gamme.couverture:
+                                             html += "Couverture: <i style='color:blue'>" + composants[u].designation + "</i><br/>";
+                                            break;
+                                    }
+                                }
+                                idProduit = produits[i].id;
+                                html += "Modules<br/>";
+                                for(var u = 0; u < produits_module.length; u++){
+                                    if(produits_module[u].cd_produit == produits[i].id){
+                                        var tarifmodule = 0;
+                                        html += "<i style='color:blue'>" + produits_module[u].cd_module.designation + "</i>";
+                                        var cptcompos = 0;
+                                         for(var n = 0; n < modules_composant.length; n++){
+                                             if(modules_composant[n].cd_module == produits_module[u].cd_module.id){
+                                                 cptcompos += parseInt(modules_composant[n].quantite);
+                                                 tarifmodule += ((parseInt(modules_composant[n].cd_composant.tarif)) * (parseInt(modules_composant[n].quantite)));
+                                                 
+                                             }
+                                         }
+                                        tarif += tarifmodule;
+                                        html += " (" + cptcompos + " composants): " + tarifmodule + " € <br/>";
+                                    }
+                                }
+                                html += "<br/>";
+                            }
+                            html += "<h3>Tarif: " + tarif + " €</h3>";
+                            pdf.create(html).toStream((err, stream) => {
+                                if (err) return res.end(err.stack)
+                                res.setHeader('Content-type', 'application/pdf')
+                                stream.pipe(res)
+                             });
+                        })
+                        
+                    })
+                })
+                    
+                
+                
+                
+            })
+            
+         })
+       
+        
+    },
+    
+    
+    'dossier': function(req, res, next){
+        Projet.findOne(req.param('id')).populate("cd_client").exec(function(err, projet){
+            if(err){
+                return next(err);
+            }
+            Produit.find({cd_projet: req.param('id')}).populate("cd_gamme").exec(function(err, produits){
+                if(err){
+                    return next(err);
+                }
+                Produit_module.find().populate("cd_module").exec(function(err, produits_module){
+                   if(err){
+                        return next(err);
+                    } 
+                    Composant.find().exec(function(err, composants){
+                       if(err){
+                            return next(err);
+                        } 
+                        Module_composant.find().populate("cd_composant").exec(function(err, modules_composant){
+                           if(err){
+                                return next(err);
+                            }
+                            var html = "";
+                            html += "<html><body><h2 style='text-align:center'>Dossier Technique</h2>";
+                            html += "<h4>Projet</h4>";
+                            html += "Référence: <i style='color:blue'>" + projet.id + "</i><br/>";
+                            html += "Projet: <i style='color:blue'>" + projet.titre + "</i><br/>";
+                            var date = new Date(projet.date_creation);
+                            html += "Date: <i style='color:blue'>" + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "</i><br/>";
+                            html += "<h4>Produits</h4>";
+                            for(var i = 0; i < produits.length; i++){
+                                html += "Produit: <i style='color:blue'>" + produits[i].note + "</i><br/>";
+                                html += "CCTP: <i style='color:blue'>Lorem Ipsum</i><br/>";
+                                for(var u = 0; u < composants.length; u++){
+                                    switch(composants[u].id){
+                                        case produits[i].cd_gamme.finition_exterieur:
+                                             html += "Finition exterieur: <i style='color:blue'>" + composants[u].designation + "</i><br/>";
+                                            break;
+                                        case produits[i].cd_gamme.finition_interieur:
+                                             html += "Finition interieur: <i style='color:blue'>" + composants[u].designation + "</i><br/>";
+                                            break;
+                                        case produits[i].cd_gamme.isolant:
+                                             html += "Isolation: <i style='color:blue'>" + composants[u].designation + "</i><br/>";
+                                            break;
+                                        case produits[i].cd_gamme.couverture:
+                                             html += "Couverture: <i style='color:blue'>" + composants[u].designation + "</i><br/>";
+                                            break;
+                                    }
+                                }
+                                idProduit = produits[i].id;
+                                html += "Modules<br/>";
+                                for(var u = 0; u < produits_module.length; u++){
+                                    if(produits_module[u].cd_produit == produits[i].id){
+                                        html += "&nbsp;&nbsp;<i style='color:blue'>" + produits_module[u].cd_module.designation + "</i><br/>";
+                                         html += "&nbsp;&nbsp;&nbsp;&nbsp;Composants<br/>";
+                                        var cptcompos = 0;
+                                         for(var n = 0; n < modules_composant.length; n++){
+                                             if(modules_composant[n].cd_module == produits_module[u].cd_module.id){
+                                                 html += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i style='color:blue'>" + modules_composant[n].cd_composant.designation + "</i> Quantité: " + modules_composant[n].quantite + "<br/>";
+                                             }
+                                         }
+                                    }
+                                }
+                                html += "<br/>";
+                            }
+                            pdf.create(html).toStream((err, stream) => {
+                                if (err) return res.end(err.stack)
+                                res.setHeader('Content-type', 'application/pdf')
+                                stream.pipe(res)
+                             });
+                        })
+                        
+                    })
+                })
+                    
+                
+                
+                
+            })
+            
+         })
+       
         
     }
 
